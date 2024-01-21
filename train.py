add mixed precision and fix regularization

train.py
@@ -1,4 +1,4 @@
-'''Train CIFAR10 with PyTorch.'''
+"""Train CIFAR10 with PyTorch."""
 from math import inf
 import numpy as np
 import random
@@ -16,18 +16,22 @@ import argparse
 
 from resnet import *
 from storm1 import *
+from tqdm import tqdm
 from utils import progress_bar
+from torch.cuda.amp import autocast, GradScaler
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 STORM1 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-parser.add_argument('--sgd', '--SGD', action='store_true',
-                    help='use SGD instead of STORM1')
+parser = argparse.ArgumentParser(description="PyTorch CIFAR10 STORM1 Training")
+parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
+parser.add_argument(
+    "--resume", "-r", action="store_true", help="resume from checkpoint"
+)
+parser.add_argument(
+    "--sgd", "--SGD", action="store_true", help="use SGD instead of STORM1"
+)
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
@@ -48,34 +52,48 @@ seed = 420  # any number
 set_deterministic(seed=seed)
 
 # Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+print("==> Preparing data..")
+transform_train = transforms.Compose(
+    [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    root="./data", train=True, download=True, transform=transform_train
+)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=128, shuffle=False, num_workers=2)
+    root="./data", train=False, download=True, transform=transform_test
+)
+testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+classes = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
 
 # Model
-print('==> Building model..')
+print("==> Building model..")
 # net = VGG('VGG19')
 # net = PreActResNet18()
 # net = GoogLeNet()
@@ -91,60 +109,86 @@ print('==> Building model..')
 # net = RegNetX_200MF()
 net = ResNet18()
 net = net.to(device)
-if device == 'cuda':
+if device == "cuda":
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
 if args.resume:
     # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
+    print("==> Resuming from checkpoint..")
+    assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found!"
+    checkpoint = torch.load("./checkpoint/ckpt.pth")
+    net.load_state_dict(checkpoint["net"])
+    best_acc = checkpoint["acc"]
+    start_epoch = checkpoint["epoch"]
 
 criterion = nn.CrossEntropyLoss()
-optimizer = STORM1(net.parameters(), lr=args.lr, momentum=0.9) if not args.sgd else optim.SGD(
-    net.parameters(), lr=args.lr, momentum=0.9, weight_decay=0)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200) if args.sgd else None
+optimizer = (
+    STORM1(net.parameters(), lr=args.lr, momentum=0.9)
+    if not args.sgd
+    else optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=0)
+)
+scheduler = (
+    optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200) if args.sgd else None
+)
+
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    print("\nEpoch: %d" % epoch)
     net.train()
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
+    pbar = tqdm(
+        enumerate(trainloader),
+        total=len(trainloader),
+        desc="Epoch {}, lr {}".format(epoch, optimizer.param_groups[0]["lr"]),
+    )
+    for batch_idx, (inputs, targets) in pbar:
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-        outputs = net(inputs)
+        with autocast(dtype=torch.float16):
+            outputs = net(inputs)
 
-        # We use L2 regularization instead of weight decay
-        l2_lambda = 1e-4
-        l2_reg = torch.tensor(0.).to(device)
-        for param in net.parameters():
-            l2_reg += torch.norm(param)
+            # We use L2 regularization instead of weight decay
+            l2_lambda = 1e-4
+            l2_reg = torch.tensor(0.0).to(device)
+            for param in net.parameters():
+                l2_reg += torch.norm(param, p=2)
+            loss = criterion(outputs, targets) + l2_lambda * l2_reg
+        scaler.scale(loss).backward()
 
-        loss = criterion(outputs, targets) + l2_lambda * l2_reg
-        loss.backward()
-
-        optimizer.step()
-
+        # scaler.step() first unscales the gradients of the optimizer's assigned params.
+        # If these gradients do not contain infs or NaNs, optimizer.step() is then called,
+        # otherwise, optimizer.step() is skipped.
+        scaler.step(optimizer)
+        # optimizer.step()
+        # Updates the scale for next iteration.
+        scaler.update()
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
         if not args.sgd:
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d) | lr: %.5f | rho: %.5f'
-                        % (train_loss/(batch_idx+1), 100.*correct/total, correct,
-                            total, optimizer.param_groups[0]['lr'], rho))
+            pbar.set_postfix(
+                {
+                    "loss": train_loss / (batch_idx + 1),
+                    "acc": 100.0 * correct / total,
+                    "rho": rho,
+                    # "lr": optimizer.param_groups[0]["lr"],
+                }
+            )
         else:
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d) | lr: %.5f'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct,
-                        total, optimizer.param_groups[0]['lr']))
+            pbar.set_postfix(
+                {
+                    "loss": train_loss / (batch_idx + 1),
+                    "acc": 100.0 * correct / total,
+                    # "lr": optimizer.param_groups[0]["lr"],
+                }
+            )
+
 
 def test(epoch):
     global best_acc
@@ -153,7 +197,8 @@ def test(epoch):
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
+        pbar = tqdm(enumerate(testloader), total=len(testloader))
+        for batch_idx, (inputs, targets) in pbar:
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
             loss = criterion(outputs, targets)
@@ -163,21 +208,25 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            pbar.set_postfix(
+                {
+                    "test_loss": test_loss / (batch_idx + 1),
+                    "test_acc": 100.0 * correct / total,
+                }
+            )
 
     # Save checkpoint.
-    acc = 100.*correct/total
+    acc = 100.0 * correct / total
     if acc > best_acc:
-        print('Saving..')
+        print("Saving..")
         state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
+            "net": net.state_dict(),
+            "acc": acc,
+            "epoch": epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
+        if not os.path.isdir("checkpoint"):
+            os.mkdir("checkpoint")
+        torch.save(state, "./checkpoint/ckpt.pth")
         best_acc = acc
 
 
@@ -186,14 +235,16 @@ outputs = net(decrease_ex[0].to(device))
 loss_prev = criterion(outputs, decrease_ex[1].to(device)).item()
 lr = args.lr
 rho = inf
-for epoch in range(start_epoch, start_epoch+200):
+# Creates a GradScaler once at the beginning of training.
+scaler = GradScaler()
+for epoch in range(start_epoch, start_epoch + 200):
     train(epoch)
     test(epoch)
 
     # Update scheduler if using SGD
     if scheduler is not None:
         scheduler.step()
-    
+
     # Compute rho every 10 epochs if using STORM1
     if epoch % 10 == 0 and not args.sgd:
         with torch.no_grad():